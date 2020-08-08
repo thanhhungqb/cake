05dc12601f42eb9c3eed7cd391e9a8288ad3d49d
Wai Kay <pottiepie@gmail.com>
2020-08-09 00:32:43 +0800

    Added localStorage and XML import/export to index

--- js/code.js
+++ js/code.js
@@ -4,13 +4,21 @@ var rtl = (document.location.search == '?rtl');
 var block = null;
 
 function start() {
-  Blockly.inject(document.getElementById('blocklyDiv'), 
+  Blockly.inject(document.getElementById('blocklyDiv'),
       {
-        path: '../', 
+        path: "./blockly/",
         toolbox: document.getElementById('toolbox')
       }
   );
   Blockly.addChangeListener(renderContent);
+  //Blockly.Blocks.CreateMainBlock();
+
+  BlocklyApps.loadBlocks('');
+
+  if ('BlocklyStorage' in window) {
+    // Hook a save function onto unload.
+    BlocklyStorage.backupOnUnload();
+  }
 }
 
 function renderContent() {
@@ -22,6 +30,10 @@ function renderContent() {
     code = prettyPrintOne(code, 'c');
     content.innerHTML = code;
   }
+  if (typeof(Storage) !== "undefined") {
+    var xml = Blockly.Xml.workspaceToDom(Blockly.mainWorkspace);
+    localStorage.setItem("Blockly.mainWorkspace", Blockly.Xml.domToText(xml));
+  }
 }
 
 /**
@@ -31,29 +43,47 @@ function discard() {
   var count = Blockly.mainWorkspace.getAllBlocks().length;
   if (count < 2 || window.confirm("Remove all blocks?")) {
     Blockly.mainWorkspace.clear();
+    //Blockly.Blocks.CreateMainBlock();
     window.location.hash = '';
   }
 }
 
-/**
- * Insert terminal into page.
- * https://github.com/jcubic/jquery.terminal
- */
-jQuery(function($, undefined) {
-    $('#terminal').terminal(function(command, term) {
-        if (command !== '') {
-            var result = window.eval(command);
-            if (result != undefined) {
-                term.echo(String(result));
-            }
-        }
-    }, {
-        greetings: 'Cake Console Terminal',
-        name: 'js_demo',
-        height: 0,
-        width: 0,
-        prompt: 'cake> '});
-});
+// /**
+//  * Insert terminal into page.
+//  * https://github.com/jcubic/jquery.terminal
+//  */
+// jQuery(function($, undefined) {
+//     $('#terminal').terminal(function(command, term) {
+//         if (command !== '') {
+//             var result = window.eval(command);
+//             if (result != undefined) {
+//                 term.echo(String(result));
+//             }
+//         }
+//     }, {
+//         greetings: 'Cake Console Terminal',
+//         name: 'js_demo',
+//         height: 0,
+//         width: 0,
+//         prompt: 'cake> '});
+// });
+
+function loadxml(xml) {
+  if (typeof xml != "string" || xml.length < 5) {
+    alert("No Input");
+    return false;
+    return;
+  }
+  try {
+    var dom = Blockly.Xml.textToDom(xml);
+    Blockly.mainWorkspace.clear();
+    Blockly.Xml.domToWorkspace(Blockly.mainWorkspace, dom);
+    return true;
+  } catch (e) {
+    alert("Invalid xml");
+    return false;
+  }
+}
 
 /**
  * Save current codes into a *.c file.
